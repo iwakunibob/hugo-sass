ba49de5da53f9ebdbde0c9707b5100ecd9066c28
iwakunibob <totorobob@protonmail.com>
2022-04-15 15:30:22 +0900

    lesson4

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+const autoprefixer = require('autoprefixer')();
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md'
+  ],
+  safelist: {
+    // standard: [],
+    // deep: [],
+    greedy: [
+      /^pagination/,
+      /^page-/
+    ]
+  }
+})
+
+module.exports = {
+  plugins: [
+    ...process.env.HUGO_ENVIRONMENT === 'development'
+      ? [null]
+      : [autoprefixer,
+        purgecss
+      ]
+  ]
+}
